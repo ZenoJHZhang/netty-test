ec2327db833b8bb5c5ced04445e92b07d622d63c
张江浩 <983165652@qq.com>
2020-08-10 15:40:51 +0800

    yyy
    (cherry picked from commit 0411cffd4882f639111c2da95fb070a1c51c7e42)

---
+++ src/main/java/com/zjh/nettytest/test/Animal.java
@@ -0,0 +1,13 @@
+package com.zjh.nettytest.test;
+
+/**
+ * Description: netty-test
+ * DateTime: 2020/8/3 9:39 上午
+ *
+ * @author 张江浩
+ */
+public class Animal {
+    public void say(){
+        System.out.println("A");
+    }
+}
